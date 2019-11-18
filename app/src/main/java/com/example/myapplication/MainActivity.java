22d9805a4d901182dbaa48b50e80180795952e41
jjjzy <yujiazhenn@hotmail.com>
2019-11-18 16:18:08 -0600

    Merge remote-tracking branch 'origin/request_service_tom' into request_service_tom
    # Conflicts:
    #	app/src/main/java/com/example/myapplication/Request_service_activity.java
    #	app/src/main/res/layout/activity_vendor_register.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //mmmmmwerwqerqwer
     EditText username;
     EditText password;
     Button btn;
