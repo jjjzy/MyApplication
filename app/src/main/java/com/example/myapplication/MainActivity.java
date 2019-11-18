3c617d9b8bedce8d78fe7f60a43b05f8533d67ce
The Queen <zhqx9019@gmail.com>
2019-11-18 15:54:45 -0600

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //jjjj
     EditText username;
     EditText password;
     Button btn;
