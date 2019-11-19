6a86cb4522948bbd8bb5b09926f544fd346996a7
Ana <aaleksandric2@txwes.edu>
2019-11-18 22:13:55 -0600

    New Version

--- app/src/main/java/com/example/myapplication/DatabaseHelper.java
+++ app/src/main/java/com/example/myapplication/DatabaseHelper.java
@@ -10,23 +10,26 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     //constructor
     public DatabaseHelper(Context context){
-        super(context,"Login.db",null,3);
+        super(context,"Login.db",null,4);
 
     }
 
     //create a table for customers
     public void onCreate(SQLiteDatabase db){
-    db.execSQL("Create table user(fname text, lname text, username text primary key, password text, answer text, address text,phone text)");
-    db.execSQL("Create table vendor(username text primary key, password text, answer text, address text,phone text,company text,service text)");
+        db.execSQL("Create table user(fname text, lname text, username text primary key, password text, answer text, address text,phone text)");
+        db.execSQL("Create table vendor(username text primary key, password text, answer text, address text,phone text,company text,service text)");
+        db.execSQL("Create table orders(user_username text,vendor_username text, service text, date text,total double)");
     }
 
     public void onUpgrade(SQLiteDatabase db, int oldVersion,int newVersion){
-    db.execSQL("drop table if exists user");
-    onCreate(db);
+        db.execSQL("drop table if exists user");
+        db.execSQL("drop table if exists vendor");
+        db.execSQL("drop table if exists orders");
+        onCreate(db);
     }
 
     //insert username and password to the database
-    public boolean insertUser(String fname, String lname, String username,String password,String answer,String address, String phone){
+    public boolean insertUser(String fname, String lname, String username,String password,String answer,String address, String phone) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put("fname", fname);
@@ -34,9 +37,24 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         contentValues.put("username", username);
         contentValues.put("password", password);
         contentValues.put("answer", answer);
-        contentValues.put("address",address);
-        contentValues.put("phone",phone);
-        long ins = db.insert("user", null,contentValues);
+        contentValues.put("address", address);
+        contentValues.put("phone", phone);
+        long ins = db.insert("user", null, contentValues);
+
+        if (ins == -1) return false;   //if the query does not work return false
+        else return true;
+    }
+
+
+    public boolean insertOrder(String user_username, String vendor_username, String service,String date,double total){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("user_username", user_username);
+        contentValues.put("vendor_username", vendor_username);
+        contentValues.put("service", service);
+        contentValues.put("date", date);
+        contentValues.put("total", total);
+        long ins = db.insert("orders", null,contentValues);
 
         if (ins==-1) return false;   //if the query does not work return false
         else return true;
