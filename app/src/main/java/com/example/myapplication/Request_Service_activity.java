0bc93825787de30aac84984dd5c76151085afbe1
jjjzy <yujiazhenn@hotmail.com>
2019-11-21 15:06:06 -0600

    dfasdf

--- app/src/main/java/com/example/myapplication/Request_Service_activity.java
+++ app/src/main/java/com/example/myapplication/Request_Service_activity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Request_Service_activity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Request_Service_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request__service_activity);
+
+        Intent in = new Intent(Request_Service_activity.this,Select_A_Service.class);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Select_A_Service.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Select_A_Service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select__a__service);
+    }
+}
