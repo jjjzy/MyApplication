bc6899eb10c6b0e81e0b73fc22a0ba08fbbc0de1
jjjzy <yujiazhenn@hotmail.com>
2019-11-21 19:35:40 -0600

    can pullout username from vendor  based on service selected

--- app/src/main/java/com/example/myapplication/View_vendors.java
+++ app/src/main/java/com/example/myapplication/View_vendors.java
@@ -3,8 +3,13 @@ package com.example.myapplication;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class View_vendors extends AppCompatActivity {
 
@@ -14,6 +19,10 @@ public class View_vendors extends AppCompatActivity {
 
     Request_Service_activity ra;
 
+    ArrayList<String> vendor_list = new ArrayList<String>();
+
+    LinearLayout ll;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,8 @@ public class View_vendors extends AppCompatActivity {
         selected_text = Request_Service_activity.selected_text;
 
         db = new DatabaseHelper(this);
+
+        ll = (LinearLayout)findViewById(R.id.ll);
 
 //        Toast.makeText(View_vendors.this, selected_text, Toast.LENGTH_SHORT).show();
 //        Log.d("Creation", selected_text.toString());
@@ -37,10 +48,23 @@ public class View_vendors extends AppCompatActivity {
             while(!cursor.isAfterLast()){
 //                Log.d("Creation", "inside while");
                 String data = cursor.getString(cursor.getColumnIndex("username"));
-                Log.d("Creation", data);
+                vendor_list.add(data);
+//                Log.d("Creation", data);
                 cursor.moveToNext();
             }
         }
         cursor.close();
+
+        for(int i = 0; i < vendor_list.size(); i++){
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT);
+            Button btn = new Button(this);
+            btn.setId(i);
+            btn.setText(vendor_list.get(i));
+//            ll.addView(btn);
+            ll.addView(btn, params);
+//            Log.d("Creation", vendor_list.get(i));
+        }
     }
 }
