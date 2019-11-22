c2961157afa0618a919fd7195cb1542d58464fcc
jjjzy <yujiazhenn@hotmail.com>
2019-11-21 23:35:13 -0600

    can pullout username from vendor  based on service selected

--- app/src/main/java/com/example/myapplication/View_vendors.java
+++ app/src/main/java/com/example/myapplication/View_vendors.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -14,13 +13,9 @@ import java.util.ArrayList;
 public class View_vendors extends AppCompatActivity {
 
     DatabaseHelper db;
-
     CharSequence selected_text;
-
     Request_Service_activity ra;
-
     ArrayList<String> vendor_list = new ArrayList<String>();
-
     LinearLayout ll;
 
     @Override
@@ -29,27 +24,15 @@ public class View_vendors extends AppCompatActivity {
         setContentView(R.layout.activity_view_vendors);
 
         selected_text = Request_Service_activity.selected_text;
-
         db = new DatabaseHelper(this);
-
         ll = (LinearLayout)findViewById(R.id.ll);
-
-//        Toast.makeText(View_vendors.this, selected_text, Toast.LENGTH_SHORT).show();
-//        Log.d("Creation", selected_text.toString());
-
         Cursor cursor = db.return_vendor(selected_text.toString());
-
         cursor.moveToFirst();
 
-//        Log.d("Creation", "begincout");
-
         if (cursor.moveToFirst()){
-//            Log.d("Creation", "inside if");
             while(!cursor.isAfterLast()){
-//                Log.d("Creation", "inside while");
-                String data = cursor.getString(cursor.getColumnIndex("username"));
+                String data = cursor.getString(cursor.getColumnIndex("company"));
                 vendor_list.add(data);
-//                Log.d("Creation", data);
                 cursor.moveToNext();
             }
         }
@@ -62,6 +45,14 @@ public class View_vendors extends AppCompatActivity {
             Button btn = new Button(this);
             btn.setId(i);
             btn.setText(vendor_list.get(i));
+
+            btn.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View view){
+//                    startActivity(new Intent(View_vendors.this, Vendor_info_popup.class));
+
+                }
+            });
+
             ll.addView(btn, params);
         }
     }
