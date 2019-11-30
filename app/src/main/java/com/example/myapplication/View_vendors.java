e7d5b5133e74734b989de6e4ec16894adb750413
The Queen <zhqx9019@gmail.com>
2019-11-29 18:04:35 -0600

    .

--- app/src/main/java/com/example/myapplication/View_vendors.java
+++ app/src/main/java/com/example/myapplication/View_vendors.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -20,6 +21,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class View_vendors extends AppCompatActivity {
+
+
 
     DatabaseHelper db;
     CharSequence selected_text;
